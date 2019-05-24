617a477ce51a7d559c021870a25579872d5d42b0
Matt Chapman <matt@mttchpmn.com>
2019-05-24 19:01:00 +1200

    Add bg color

---
+++ Theme.js
@@ -0,0 +1,11 @@
+module.exports = {
+  primary: "#E91E63",
+  primaryDark: "#C2185B",
+  primaryLight: "#F8BBD0",
+  accent: "#9E9E9E",
+  primaryText: "#212121",
+  secondaryText: "#757575",
+  white: "#FFFFFF",
+  divider: "#BDBDBD",
+  background: "#EEEEEE"
+};
