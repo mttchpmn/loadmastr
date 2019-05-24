446619fd76cc52095d1ee2acfd13f0ce73cbcf19
Matt Chapman <matt@mttchpmn.com>
2019-05-24 19:31:29 +1200

    Update design and layout

--- screens/Home/LoadsheetItem.js
+++ screens/Home/LoadsheetItem.js
@@ -1,22 +1,59 @@
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
+
 import Theme from "../../Theme.js";
+import Style from "../../Style.js";
 
 const LoadsheetItem = ({ loadsheetData }) => {
   return (
     <View
       style={{
         width: "98%",
-        height: 60,
+        height: 80,
+        backgroundColor: Theme.primaryLight,
         borderRadius: 2,
-        padding: 7,
         margin: 4,
-        alignSelf: "center",
-        backgroundColor: Theme.primaryLight
+        alignSelf: "center"
       }}
     >
-      <TouchableOpacity>
-        <Text>{loadsheetData.flightDetails.date}</Text>
+      <TouchableOpacity style={{ height: "100%", width: "100%" }}>
+        <View
+          style={{
+            height: "100%",
+            width: "100%",
+            padding: 7,
+            justifyContent: "space-around"
+          }}
+        >
+          <View style={[Style.flexRow]}>
+            <View style={{ width: "50%" }}>
+              <Text style={{ textAlign: "left", fontSize: 14 }}>
+                {loadsheetData.flightDetails.date}
+              </Text>
+            </View>
+
+            <View style={{ width: "50%" }}>
+              <Text style={{ textAlign: "right", fontSize: 14 }}>
+                {loadsheetData.flightDetails.aircraftRego} /{" "}
+                {loadsheetData.flightDetails.pilot}
+              </Text>
+            </View>
+          </View>
+
+          <View style={[Style.flexRow]}>
+            <View style={{ width: "50%" }}>
+              <Text style={{ textAlign: "left", fontSize: 14 }}>
+                {loadsheetData.flightDetails.route}
+              </Text>
+            </View>
+
+            <View style={{ width: "50%" }}>
+              <Text style={{ textAlign: "right", fontSize: 14 }}>
+                {loadsheetData.paxDetails.length} Pax
+              </Text>
+            </View>
+          </View>
+        </View>
       </TouchableOpacity>
     </View>
   );
