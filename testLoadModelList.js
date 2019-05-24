3c5c9800dd67f484a48f5a4f5133b8041a338996
Matt Chapman <matt@mttchpmn.com>
2019-05-24 19:02:25 +1200

    Update data size

--- testLoadModelList.js
+++ testLoadModelList.js
@@ -228,5 +228,695 @@ module.exports = [
       burnWeight: 0,
       landingWeight: 0
     }
+  },
+  {
+    flightDetails: {
+      date: "date ONE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLD",
+      pilot: "J Yang",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date TWO",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLP",
+      pilot: "E Kensington",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date THREE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLQ",
+      pilot: "M Chapman",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date FOUR",
+      route: "QN - MF//",
+      aircraftType: "C172",
+      aircraftRego: "ZK-JMG",
+      pilot: "T Muirhead",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date FIVE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLW",
+      pilot: "M Rutherford",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date ONE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLD",
+      pilot: "J Yang",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date TWO",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLP",
+      pilot: "E Kensington",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date THREE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLQ",
+      pilot: "M Chapman",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date FOUR",
+      route: "QN - MF//",
+      aircraftType: "C172",
+      aircraftRego: "ZK-JMG",
+      pilot: "T Muirhead",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date FIVE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLW",
+      pilot: "M Rutherford",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date ONE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLD",
+      pilot: "J Yang",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date TWO",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLP",
+      pilot: "E Kensington",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date THREE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLQ",
+      pilot: "M Chapman",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date FOUR",
+      route: "QN - MF//",
+      aircraftType: "C172",
+      aircraftRego: "ZK-JMG",
+      pilot: "T Muirhead",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
+  },
+  {
+    flightDetails: {
+      date: "date FIVE",
+      route: "QN - MF//",
+      aircraftType: "GA8",
+      aircraftRego: "ZK-SLW",
+      pilot: "M Rutherford",
+      coPilot: null
+    },
+    paxDetails: [
+      {
+        name: "John Smith",
+        weight: 83,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      },
+      {
+        name: "Sally Smith",
+        weight: 56,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: true,
+        infantWeight: 13
+      },
+      {
+        name: "Carl Smith",
+        weight: 36,
+        hasBaggage: false,
+        baggageWeight: 0,
+        hasInfant: false,
+        infantWeight: 0
+      }
+    ],
+    flightTotals: {
+      basicEmptyWeight: 0,
+      crewWeight: 0,
+      paxWeight: 0,
+      zeroFuelWeight: 0,
+      fuelWeight: 0,
+      takeoffWeight: 0,
+      burnWeight: 0,
+      landingWeight: 0
+    }
   }
 ];
